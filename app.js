8f0e09e635f2da8c7a4e88a5d9ab386e0dec0ea6
ilanaenderman <ilanaenderman@hotmail.com>
2017-02-17 10:35:32 +0100

    bcrypt works with mongoose

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 // require libraries
-const User    = require('./data/base/database.js');
+const User    = require(__dirname + '/data/db.js');
 const os      = require('os');
 const cluster = require('cluster');
 
-// coe to run if we're in a master process
-if (cluster.isMaster) {
 
-    // Create a worker for each CPU
-    for (var i = 0; i < os.cpus().length; i ++) {
-        var worker = cluster.fork();
-        console.log("started working with pid%d", worker.process.pid);
-    }
+// // code to run if we're in a master process
+// if (cluster.isMaster) {
+//
+//     // Create a worker for each CPU
+//     for (var i = 0; i < os.cpus().length; i ++) {
+//         var worker = cluster.fork();
+//         console.log("started working with pid%d", worker.process.pid);
+//     }
+//
+//     cluster.on('exit', (worker, code, signal) => {
+//         console.log('Worker died', worker.id);
+//         worker;
+//     })
+//
+// // Code to run if we're in a worker process
+// } else {
+//
+//     // Include Express
+//     var express = require('express');
+//
+//     // Create a new Express application
+//     var app = express();
+//
+//     // Add a basic route – index page
+//     app.get('/', (req, res) => {
+//         res.send('Hello from worker' + cluster.worker.id);
+//     });
+//
+//     // Bind to a port
+//     app.listen(8000);
+//     console.log('Server connected to port 8000', cluster.worker.id);
+// }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log('Worker died', worker.id);
-        worker;
-    })
-
-// Code to run if we're in a worker process
-} else {
-
-    // Include Express
-    var express = require('express');
-
-    // Create a new Express application
-    var app = express();
-
-    // Add a basic route – index page
-    app.get('/', (req, res) => {
-        res.send('Hello from worker' + cluster.worker.id);
-    });
-
-    // Bind to a port
-    app.listen(8000);
-    console.log('Server connected to port 8000', cluster.worker.id);
-}
+const express = require('express');
+const app     = express();
 
 
 
+app.get('/', function(req, res) {
+    res.send('Hello world!');
+});
+
+// Bind to a port
+app.listen(8000);
+console.log('Server connected to port 8000');
---
+++ data/db.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-node');
+
+var userSchema = new mongoose.Schema({
+    username:{type: String, unique: true},
+    password:String
+});
+
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.hash(user.password, null, null, function(err, hash){
+            if (err){
+                next();
+            }
+            user.password = hash;
+            next();
+        });
+    }
+    next();
+})
+
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+        callback(isMatch);
+    });
+};
+var User = mongoose.model('users', userSchema);
+
+
+// connect mongoose
+mongoose.connect('mongodb://localhost/mongoose-bcrypt-test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('We are connected to the database!')
+});
+
+// CREATE TEST USER
+var testUser = new User({
+    name: "Yara Enderman",
+    username: 'yaartje',
+    password: 'password123',
+    admin: true
+});
+
+// SAVE user to database
+testUser.save(function(err, data){
+    if(err) console.log(err);
+    else console.log ('Sucess:' , data);
+});
+
+module.exports = User;
